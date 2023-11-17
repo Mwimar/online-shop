fbfef8f6184b78361df166cbfc7c0df608f73f1c
Mark Mwima <markmwimar@gmail.com>
2023-11-17 21:12:23 +0300

    enabling error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./data/database");
+const errorHandlerMiddleware=require('./middlewares/error-handler')
 
 const mongodb = require("mongodb");
 
@@ -10,12 +11,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
 const authRoutes = require("./routes/auth.routes");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));//only supports regular form submission
 
 app.use(authRoutes);
+app.use(errorHandlerMiddleware)
 
 db.connectToDatabase()
   .then(function () {
