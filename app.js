6717c6add858386fb0bfb866109265383b82b923
Mark Mwima <markmwimar@gmail.com>
2023-11-19 20:40:10 +0300

    sessing sessions

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const db = require("./data/database");
-const errorHandlerMiddleware=require('./middlewares/error-handler')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+const createSessionConfig=require('./config/sessions')
 
 const mongodb = require("mongodb");
 
-const session = require("express-session");
+const expressSession = require("express-session");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -16,12 +17,14 @@ const authRoutes = require("./routes/auth.routes");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));//only supports regular form submission
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig))
 
 app.use(authRoutes);
 app.use(errorHandlerMiddleware)
 
 db.connectToDatabase()
-  .then(function () {
+  .then(function () { 
   app.listen(3000);
   })
   .catch(function (error) {
