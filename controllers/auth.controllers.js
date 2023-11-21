5f7b28c99b4b6027bb0318112f1193ab1ef05522
Mark Mwima <markmwimar@gmail.com>
2023-11-21 19:17:53 +0300

    validation functions

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model')
 
 const authUtil = require("../util/authentication");
+const userCredentialsAreValid=require('')
 
 const mongodb = require('mongodb')
 
