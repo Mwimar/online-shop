82c7e2dae201c161bbbc599d40040e6666aa6694
Mark Mwima <markmwimar@gmail.com>
2023-11-24 19:16:31 +0300

    toggling mobile menu

---
+++ public/scripts/mobile.js
@@ -0,0 +1,6 @@
+const mobileMenuBtnElement = document.getElementById('mobile-menu-btn-');
+const mobileMenu = document.getElementById('mobile-menu');
+
+function toggleMobileMenu() { }
+
+mobileMenuBtnElement.addEventListener('click', toggleMobileMenu)
