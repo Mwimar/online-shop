2a61ba86ab7c51a437bb88b881f5dc6fac6055e2
Mark Mwima <markmwimar@gmail.com>
2023-11-25 11:29:25 +0300

    enabling image upload middleware

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const router = express.Router();
 
-const adminController=require('../controllers/admin.controllers')
+const adminController = require('../controllers/admin.controllers')
+
+const imageUploadMiddleware = require('../middlewares/image-upload');
+const router = express.Router();
 
 router.get('/products', adminController.getProducts);
 
