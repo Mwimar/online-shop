61494b7c0a04ce6f80ed45277e501b705dfa3151
Mark Mwima <markmwimar@gmail.com>
2023-11-25 19:20:41 +0300

    saving product price as a number

--- models/product.model.js
+++ models/product.model.js
@@ -3,7 +3,7 @@ class Product {
     constructor(productData) {
         this.title = productData.title;
         this.summary = productData.summary;
-        this.price = productData.price;
+        this.price = +productData.price;
         this.description = productData.description;
         this.image = productData.image;//name of image file
         this.imagePath = `product-data/images/${productData.image}`;
