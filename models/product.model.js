b48ac6b4d259a5a9997d6d10a29fb1d2833e5253
Mark Mwima <markmwimar@gmail.com>
2023-11-25 18:47:28 +0300

    product model costructor

---
+++ models/product.model.js
@@ -0,0 +1,13 @@
+class Product {
+    constructor(productData) {
+        this.title = productData.title;
+        this.summary = productData.summary;
+        this.price = productData.price;
+        this.description = productData.description;
+        this.image = productData.image;//name of image file
+        this.imagePath = `product-data/images/${productData.image}`;
+        this.imageUrl = `/products/assets/images/${productData.image}`;
+    }
+}
+
+module.exports = Product;
