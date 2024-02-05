07c72f64caa5a97a389cf4a05d3dfd05d294849b
Mark Mwima <markmwimar@gmail.com>
2024-02-06 02:23:42 +0300

    async function

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,4 +1,14 @@
-function addCartItem() { }
+const Product = require('../models/product.model');
+
+ async function addCartItem(req, res) {
+    let product;
+    try {
+        product = await Product.findById(req.body.productId);        
+    } catch (error) {
+        next(error);
+        return;
+    }
+ }
  
 module.exports = {
     addCartItem: addCartItem
