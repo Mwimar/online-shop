42c01ce66ce9b65aa6c981d28130fd40faa534e0
Mark Mwima <markmwimar@gmail.com>
2024-02-06 00:46:18 +0300

    adding if check

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -4,8 +4,12 @@ class Cart {
     }
 
     addItem(product) {
+        
         for (let i = 0; i < this.items.length; i++){
             const item = this.items[i];
+            if (item.product.id === product.id) {
+                
+            }
         }
         this.items.push(product);
     }
