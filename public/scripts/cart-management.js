871d18b0ee3cd6befaae05fa3ff8eb8853719f75
Mark Mwima <markmwimar@gmail.com>
2024-02-08 23:36:16 +0300

    fetch method

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,6 +1,13 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
 
-function addToCart() {
+async function addToCart() {
+    const productId = addToCartButtonElement.dataset.productid;
+    fetch('/cart/items', {
+        method: 'POST',
+        body: JSON.stringify({
+            productId: productId
+        }),
+    })
     
 }
 
