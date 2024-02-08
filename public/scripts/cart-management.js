e2598785b739a90aa149414bf16a9dcc181d6eba
Mark Mwima <markmwimar@gmail.com>
2024-02-08 23:53:16 +0300

    updating cart badge

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,5 +1,5 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
-const cartBadgeElement = document.querySelector('.nav-items .badge');
+const cartBadgeElement = document.querySelector('.nav-items .badge'); 
 
 async function addToCart() {
     const productId = addToCartButtonElement.dataset.productid;
@@ -27,6 +27,7 @@ async function addToCart() {
 
     const responseData = await response.json();
     const newTotalQuantity = responseData.newTotalItems;
+    cartBadgeElement.textContent = newTotalQuantity;
     
 }
 
