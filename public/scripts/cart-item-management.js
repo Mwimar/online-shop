7176987bc93d6a75c890d62c39e4cfbc7687a396
Mark Mwima <markmwimar@gmail.com>
2024-02-13 02:16:15 +0300

    accessing new quantity

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -6,6 +6,7 @@ function updateCartItem(event) {
     const form = event.target;
 
     const productId = form.dataset.productid;
+    const quantity = form.firstElementChild.value;
 }
 
 for (const formElement of cartItemUpdateFormElements) {
