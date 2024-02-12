7190626d04735ba370f56d69cd186978d3147604
Mark Mwima <markmwimar@gmail.com>
2024-02-13 02:07:58 +0300

    update cart item function

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -1,5 +1,7 @@
 const cartItemUpdateFormElements = document.querySelectorAll('.cart-item-management');
 
+function updateCartItem(){}
+
 for (const formElement of cartItemUpdateFormElements) {
     formElement.addEventListener('submit');
 }
