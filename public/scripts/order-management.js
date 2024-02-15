b0996d783c2968b4a5c99ae3a193cd97bc8fff9c
Mark Mwima <markmwimar@gmail.com>
2024-02-16 01:14:26 +0300

    update order method

--- public/scripts/order-management.js
+++ public/scripts/order-management.js
@@ -1,2 +1,10 @@
 const updateOrderFirmElements = document.querySelectorAll('.order-actions form');
 
+async function updateOrder(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const formData = new FormData(form);
+    const newStatus = formData.get('status');
+    const orderId = formData.get('orderid');
+}
