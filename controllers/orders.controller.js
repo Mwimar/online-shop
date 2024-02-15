eebbafe286e7e4fbd11e7d51238d9a3f37587672
Mark Mwima <markmwimar@gmail.com>
2024-02-15 20:52:44 +0300

    saving order

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,9 +1,22 @@
 const Order = require('../models/order.model');
+const User = require('../models/user.model');
 
-function addOrder(req, res) {
+async function addOrder(req, res,next) {
     const cart = res.locals.cart;
-    const order=new Order(cart, )
- };
+    let userDocument;
+    try {
+        userDocument = await User.findById(res.locals.uid);
+    } catch (error) {
+        return next(error);
+    }
+    const order = new Order(cart, userDocument);
+    try {
+        order.save();
+    } catch (error) {
+        return next(error);
+     }
+        
+    }
 
 module.exports = {
     addOrder:addOrder
