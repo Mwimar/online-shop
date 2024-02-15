acac1d95785917dda13f4b837795ea21eb56399f
Mark Mwima <markmwimar@gmail.com>
2024-02-16 01:19:10 +0300

    error handling

--- public/scripts/order-management.js
+++ public/scripts/order-management.js
@@ -7,4 +7,21 @@ async function updateOrder(event) {
     const formData = new FormData(form);
     const newStatus = formData.get('status');
     const orderId = formData.get('orderid');
+
+    let response;
+
+    try {
+        response = await fetch(`/admin/orders/${orderId}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                newStatus: newStatus,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        alert('Something went wrong - could not update order status.');
+        return;
+    }
 }
